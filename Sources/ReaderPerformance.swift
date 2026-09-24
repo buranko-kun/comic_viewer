49b33ca687b10bee563f7c02f5395e13be6f2e14
Esteban <blazquez.e@gmail.com>
2026-09-24 13:49:45 +0900

    Fix unified logging API usage

--- Sources/ReaderPerformance.swift
+++ Sources/ReaderPerformance.swift
@@ -1,5 +1,5 @@
 import Foundation
-import os.log
+import os
 
 /// Unified performance instrumentation for the reader.
 ///
@@ -7,17 +7,18 @@ import os.log
 /// Use Console/Instruments with subsystem `com.esteban.ComicViewer` and category `Performance`.
 enum ReaderPerformance {
     static let subsystem = "com.esteban.ComicViewer"
-    static let log = OSLog(subsystem: subsystem, category: "Performance")
+    static let log = Logger(subsystem: subsystem, category: "Performance")
+    private static let signpostLog = OSLog(subsystem: subsystem, category: "Performance")
 
     @discardableResult
     static func begin(_ name: StaticString) -> OSSignpostID {
-        let id = OSSignpostID(log: log)
-        os_signpost(.begin, log: log, name: name, signpostID: id)
+        let id = OSSignpostID(log: signpostLog)
+        os_signpost(.begin, log: signpostLog, name: name, signpostID: id)
         return id
     }
 
     static func end(_ name: StaticString, _ id: OSSignpostID) {
-        os_signpost(.end, log: log, name: name, signpostID: id)
+        os_signpost(.end, log: signpostLog, name: name, signpostID: id)
     }
 
     static func event(_ message: String) {
